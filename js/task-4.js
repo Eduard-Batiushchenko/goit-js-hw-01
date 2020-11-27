9a4c0e9f899747775c56512735c5502e8ad16d1a
Eduard Batiushchenko <72995778+Eduard-Batiushchenko@users.noreply.github.com>
2020-11-27 22:51:26 +0200

    Сделал 4.5 задание

---
+++ js/task-4.js
@@ -0,0 +1,17 @@
+let total = 23580;
+const pricePerDroid = 3000;
+let input = prompt('Введите количество дроидов, которые вы хотите купить');
+
+const totalPrice = input * pricePerDroid;
+
+if (input === null) {
+  console.log('Отменено пользователем!');
+} else if (totalPrice <= total) {
+  input = Number(input);
+  total = total - totalPrice;
+  console.log(
+    `Вы купили ${input} дроидов, на счету осталось ${total} кредитов.`,
+  );
+} else {
+  console.log('Недостаточно средств на счету!');
+}
